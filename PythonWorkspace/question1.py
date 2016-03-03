67f86dbd18768b08d27b8cc7034cc55d784fd95d
youralien <ryan.louie@students.olin.edu>
2016-03-03 09:40:31 +0800

    successful connection and unittest move forward

--- PythonWorkspace/question1.py
+++ PythonWorkspace/question1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt #used for image plotting
 import signal
 
 from idash import IDash
-
+from robot_helpers import vomega2bytecodes
 
 class GracefulKiller:
     kill_now = False
@@ -19,7 +19,11 @@ class GracefulKiller:
 
 class FinalProjectProgram():
 
-    def __init__(self):
+    def __init__(self, color, number):
+        # parameter init
+        self.bot_name = '%s%d' % (color, number)
+
+        # run startup methods
         self.initialize_vrep_client()
         self.initilize_vrep_api()
         self.killer = GracefulKiller()
@@ -45,45 +49,46 @@ class FinalProjectProgram():
             vrep.simxFinish(self.clientID)
 
     def initilize_vrep_api(self):
-        # initialize ePuck handles and variables
-        _, self.bodyelements=vrep.simxGetObjectHandle(
-            self.clientID, 'ePuck_bodyElements', vrep.simx_opmode_oneshot_wait)
+        # initialize bot handles and variables
         _, self.leftMotor=vrep.simxGetObjectHandle(
-            self.clientID, 'ePuck_leftJoint', vrep.simx_opmode_oneshot_wait)
+            self.clientID, '%s_leftJoint' % self.bot_name, vrep.simx_opmode_oneshot_wait)
         _, self.rightMotor=vrep.simxGetObjectHandle(
-            self.clientID, 'ePuck_rightJoint', vrep.simx_opmode_oneshot_wait)
-        _, self.ePuck=vrep.simxGetObjectHandle(
-            self.clientID, 'ePuck', vrep.simx_opmode_oneshot_wait)
-        _, self.ePuckBase=vrep.simxGetObjectHandle(
-            self.clientID, 'ePuck_base', vrep.simx_opmode_oneshot_wait)
+            self.clientID, '%s_rightJoint' % self.bot_name, vrep.simx_opmode_oneshot_wait)
+        _, self.bot=vrep.simxGetObjectHandle(
+            self.clientID, self.bot_name, vrep.simx_opmode_oneshot_wait)
         # proxSens = prox_sens_initialize(self.clientID)
-        # initialize odom of ePuck
-        _, self.xyz = vrep.simxGetObjectPosition(
-            self.clientID, self.ePuck, -1, vrep.simx_opmode_streaming)
-        _, self.eulerAngles = vrep.simxGetObjectOrientation(
-            self.clientID, self.ePuck, -1, vrep.simx_opmode_streaming)
+        # initialize odom of bot
+        # _, self.xyz = vrep.simxGetObjectPosition(
+        #     self.clientID, self.bot, -1, vrep.simx_opmode_streaming)
+        # _, self.eulerAngles = vrep.simxGetObjectOrientation(
+        #     self.clientID, self.bot, -1, vrep.simx_opmode_streaming)
 
-        # initialize overhead cam
-        _, self.overheadCam=vrep.simxGetObjectHandle(
-            self.clientID, 'Global_Vision', vrep.simx_opmode_oneshot_wait)
-        _, self.resolution, self.image = vrep.simxGetVisionSensorImage(
-            self.clientID,self.overheadCam,0,vrep.simx_opmode_oneshot_wait)
+        # # initialize overhead cam
+        # _, self.overheadCam=vrep.simxGetObjectHandle(
+        #     self.clientID, 'Global_Vision', vrep.simx_opmode_oneshot_wait)
+        # _, self.resolution, self.image = vrep.simxGetVisionSensorImage(
+        #     self.clientID,self.overheadCam,0,vrep.simx_opmode_oneshot_wait)
 
-        # initialize goal handle + odom
-        _, self.goalHandle=vrep.simxGetObjectHandle(
-            self.clientID, 'Goal_Position', vrep.simx_opmode_oneshot_wait)
-        _, self.goalPose = vrep.simxGetObjectPosition(
-            self.clientID, self.goalHandle, -1, vrep.simx_opmode_streaming)
+        # # initialize goal handle + odom
+        # _, self.goalHandle=vrep.simxGetObjectHandle(
+        #     self.clientID, 'Goal_Position', vrep.simx_opmode_oneshot_wait)
+        # _, self.goalPose = vrep.simxGetObjectPosition(
+        #     self.clientID, self.goalHandle, -1, vrep.simx_opmode_streaming)
 
-        # STOP Motor Velocities.  Not sure if this is necessary
-        _ = vrep.simxSetJointTargetVelocity(
-            self.clientID,self.leftMotor,0,vrep.simx_opmode_oneshot_wait)
-        _ = vrep.simxSetJointTargetVelocity(
-            self.clientID,self.rightMotor,0,vrep.simx_opmode_oneshot_wait)
-
-    def robot_code():
+    def robot_code(self):
         """ OUR ROBOT CODE GOES HERE """
-        pass
+        while True:
+            self.unittest_move_forward()
+
+    def unittest_move_forward(self):
+        forward_vel = 1
+        omega = 0
+        ctrl_sig_left, ctrl_sig_right = vomega2bytecodes(forward_vel, omega, g=1)
+        _ = vrep.simxSetJointTargetVelocity(
+            self.clientID,self.leftMotor,ctrl_sig_left,vrep.simx_opmode_oneshot_wait) # set left wheel velocity
+        _ = vrep.simxSetJointTargetVelocity(
+            self.clientID,self.rightMotor,ctrl_sig_right,vrep.simx_opmode_oneshot_wait) # set right wheel velocity
+
 
     def clean_exit(self):
         _ = vrep.simxStopSimulation(self.clientID,vrep.simx_opmode_oneshot_wait)
@@ -98,5 +103,5 @@ class FinalProjectProgram():
         self.clean_exit()
 
 if __name__ == '__main__':
-    obj = FinalProjectProgram()
+    obj = FinalProjectProgram('Blue', 1)
     obj.run()
