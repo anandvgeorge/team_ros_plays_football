12405f50464e8f26deec0dbbb652c455c0e6fc1f
youralien <ryan.louie@students.olin.edu>
2016-03-11 15:18:18 +0800

    Add delay method so they dont bump into each other while navigating to the zone

--- PythonWorkspace/zone_passer.py
+++ PythonWorkspace/zone_passer.py
@@ -14,20 +14,30 @@ class ZonePasser(base_robot.BaseRobotRunner):
 
     def __init__(self, *args, **kwargs):
         super(ZonePasser, self).__init__(*args, **kwargs)
+        self.delay = 0
 
     def add_zone_destination(self, number):
         index = number - 1 # 0 vs 1 indexing
         self.path = self.zone_locations[:, index].reshape(-1, 1)
 
+    def add_delay(self, delay):
+        self.delay = delay
+
     def robotCode(self):
         print("%s started" % self.bot_name)
         t0 = time.time()
+        time.sleep(self.delay)
         while time.time() - t0 < 20:
             robotConf = self.getRobotConf(self.bot)
             self.followPath(robotConf)
         print("%s finished" % self.bot_name)
 
-runner = base_robot.MultiRobotRunner()
+class ZonePasserMaster(base_robot.MultiRobotRunner):
+
+    def __init__(self, *args, **kwargs):
+        super(ZonePasserMaster, self).__init__(*args, **kwargs)
+
+runner = ZonePasserMaster(ip='127.0.0.1')
 
 print "runnerClientID", runner.clientID
 bot1 = ZonePasser(color='Blue', number=1, clientID=runner.clientID)
@@ -36,10 +46,12 @@ runner.addRobot(bot1)
 
 bot2 = ZonePasser(color='Blue', number=2, clientID=runner.clientID)
 bot2.add_zone_destination(4)
+bot2.add_delay(1)
 runner.addRobot(bot2)
 
 bot3 = ZonePasser(color='Blue', number=3, clientID=runner.clientID)
 bot3.add_zone_destination(3)
+bot3.add_delay(2)
 runner.addRobot(bot3)
 
 runner.run()
