f7a6435eb513c9059e9919163396afc80e73d281
youralien <ryan.louie@students.olin.edu>
2016-04-10 12:23:33 +0800

    Fix error with vels

--- PythonWorkspace/base_robot.py
+++ PythonWorkspace/base_robot.py
@@ -243,6 +243,10 @@ class BaseRobotRunner(object):
                 self.getRobotConf(self.bot),
                 self.passivePos,
                 r=0.01)
+            v = 10*np.ones((1, np.size(self.path,1)))
+            self.path = np.concatenate((self.path, v), axis=0)
+            self.path[-1, -1]=vf
+
 
             self.multiObstacleAwarePath(obstacleConfs, 0.07)
             self.prunePath()
