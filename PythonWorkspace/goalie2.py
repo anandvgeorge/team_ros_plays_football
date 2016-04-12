8a1ad3ac1ebda6826b88da5f1968b9f5c3de6268
youralien <ryan.louie@students.olin.edu>
2016-04-12 21:39:32 +0800

    changed goalie2 to have the newest

--- PythonWorkspace/goalie2.py
+++ PythonWorkspace/goalie2.py
@@ -21,20 +21,9 @@ class MyRobotRunner(base_robot.BaseRobotRunner):
         super(MyRobotRunner, self).__init__(*args, **kwargs)
 
     def robotCode(self):
-                                
-#        dash = IDash(framerate=0.1)            
-#        plt.plot(self.path[0,:], self.path[1,:])  
-#        dash.add(plt)
-        
-        #k=0.0345    # ball model: d(t) = vmot*k*(1-exp(-t/T))
-
-#        while (self.ballEngine.getSimTime()-t)<10: #End program after 30sec
-        t = time.time()
-        while time.time()-t<1000:
-            robotConf = self.getRobotConf(self.bot)  
-            self.keepGoal(robotConf, y=0.65, goalLim=0.2)
-            time.sleep(0.01)  #s
-        
+        while True:
+            robotConf = self.getRobotConf(self.bot)
+            self.keepGoalP(robotConf)
         self.setMotorVelocities(0,0)
 
 
